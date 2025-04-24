57adc65fdeaafcc9840c973f235ea8b174f127f7
Marcin Wardzinski <wardzinskim@outlook.com>
2025-04-24 17:52:26 +0200

    add TodoTask domain methods

---
+++ ToDoApp.Domain/ToDoTask/Rules/ToDoTaskPercentageCompletionMustBeInRange.cs
@@ -0,0 +1,16 @@
+﻿using ToDoApp.SharedKernel;
+
+namespace ToDoApp.Domain.ToDoTask.Rules;
+
+public class ToDoTaskPercentageCompletionMustBeInRange(int percentage) : IBusinessRule
+{
+    private static readonly Error Error =
+        new BusinessRuleValidationError(nameof(ToDoTaskPercentageCompletionMustBeInRange),
+            "Percentage must be between 0 and 100.");
+
+    public Result Validate()
+    {
+        if (percentage is > 0 and < 100) return Result.Success();
+        return Result.Failure(Error);
+    }
+}
--- ToDoApp.Domain/ToDoTask/ToDoTask.cs
+++ ToDoApp.Domain/ToDoTask/ToDoTask.cs
@@ -1,4 +1,5 @@
 ﻿using ToDoApp.Domain.ToDoTask.Events;
+using ToDoApp.Domain.ToDoTask.Rules;
 using ToDoApp.SharedKernel;
 
 namespace ToDoApp.Domain.ToDoTask;
@@ -6,19 +7,21 @@ namespace ToDoApp.Domain.ToDoTask;
 public class ToDoTask : Entity, IAggregateRoot, IAuditable
 {
     public ToDoTaskId Id { get; init; }
-    public string Title { get; init; }
-    public string? Description { get; init; }
-    public byte PercentageCompletion { get; init; }
+    public string Title { get; private set; }
+    public string? Description { get; private set; }
+    public int PercentageCompletion { get; private set; }
+    public DateTime ExpirationDateTime { get; private set; }
 
     public DateTime CreationDate { get; }
     public DateTime? LastUpdated { get; }
 
 
-    private ToDoTask(ToDoTaskId id, string title, string? description)
+    private ToDoTask(ToDoTaskId id, string title, string? description, DateTime expirationDateTime)
     {
         Id = id;
         Title = title;
         Description = description;
+        ExpirationDateTime = expirationDateTime;
         PercentageCompletion = 0;
 
         AddDomainEvent(new ToDoTaskCreatedEvent(Id));
@@ -28,12 +31,33 @@ public class ToDoTask : Entity, IAggregateRoot, IAuditable
     public static Result<ToDoTask> Create(
         IIdGenerator idGenerator,
         string title,
-        string? description
+        string? description,
+        DateTime expirationDateTime
     )
     {
         var todoTaskId = ToDoTaskId.Of(idGenerator.NextId());
         if (todoTaskId.IsFailure) return todoTaskId.Error;
 
-        return new ToDoTask(todoTaskId.Value, title, description);
+        return new ToDoTask(todoTaskId.Value, title, description, expirationDateTime);
+    }
+
+    public Result Update(string title, string? description)
+    {
+        Title = title;
+        Description = description;
+
+        return Result.Success();
+    }
+
+    public Result MarkAsDone() => SetPercentageCompletion(100);
+
+    public Result SetPercentageCompletion(byte percentage)
+    {
+        var result = CheckRules(new ToDoTaskPercentageCompletionMustBeInRange(percentage));
+        if (result.IsFailure) return result;
+
+        PercentageCompletion = percentage;
+
+        return Result.Success();
     }
 }
