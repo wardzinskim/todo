0c849c982d177a9e8cdb1c285f5c45d88dd0098f
Marcin Wardzinski <wardzinskim@outlook.com>
2025-04-25 10:33:06 +0200

    add db migrations

---
+++ ToDoApp.Infrastructure/Migrations/20250425083232_Initial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "todo_app");
+
+            migrationBuilder.CreateTable(
+                name: "todos",
+                schema: "todo_app",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    description = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    percentage_completion = table.Column<int>(type: "integer", nullable: false),
+                    expiration_date_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_todos", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "todos",
+                schema: "todo_app");
+        }
+    }
+}
